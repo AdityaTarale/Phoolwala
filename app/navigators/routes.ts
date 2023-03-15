5898e0752af1db47f4d164a5108ec95a944a30db
AdityaTarale <tarale.adi@gmail.com>
2023-03-15 16:38:11 +0530

    Add: Routes and role constants

--- app/navigators/routes.ts
+++ app/navigators/routes.ts
@@ -1,11 +1,27 @@
-export enum ROUTES {
-  Demo = "Demo",
-  Welcome = "Welcome",
+export const ROUTES = {
+  Demo: "Demo",
+  Welcome: "Welcome",
 
   // Auth
-  Login = "Login",
-  DemoShowroom = "DemoShowroom",
-  DemoCommunity = "DemoCommunity",
-  DemoPodcastList = "DemoPodcastList",
-  DemoDebug = "DemoDebug",
+  Login: "Login",
+  DemoShowroom: "DemoShowroom",
+  DemoCommunity: "DemoCommunity",
+  DemoPodcastList: "DemoPodcastList",
+  DemoDebug: "DemoDebug",
+
+  // User
+  User: {},
+  // Merchant
+  Merchant: {},
+  // Delivery Boy
+  DeliveryBoy: {},
+  // Admin
+  Admin: {},
+}
+
+export enum ROLES {
+  User = "user",
+  Merchant = "merchant",
+  DeliveryBoy = "deliveryBoy",
+  Admin = "admin",
 }
